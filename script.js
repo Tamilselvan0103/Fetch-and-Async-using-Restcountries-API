eac321deb508734c7991d1558eec7e0493175179
Tamilselvan0103 <tamilselvans0103@gmail.com>
2023-01-03 16:13:49 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,45 @@
+
+const row = document.querySelector('#mainrow')
+
+function restcountries() {
+  let countrydetails = fetch("https://restcountries.com/v3.1/all");
+  countrydetails.then((res) => res.json())
+    .then((data) => {
+      showCountry(data);
+    });
+}
+restcountries();
+
+
+function showCountry(data1) {
+  const cardDetails = data1.map((data1,index) => {
+    return `
+    <div class="card content col-lg-4 col-sm-12" style="width: 18rem;">
+    <h1 class = "card-title text-center" id ="title">${data1.name.common}</h1>
+    <img src="${data1.flags.png}" class="img-fluid img" alt="${data1.name.common}" >
+    <div class="card-body">
+    <p><span class = "card-title">Capital : </span>${data1.capital}</p>
+    <p><span class = "card-title">Region : </span>${data1.region}</p>
+    <p><span class = "card-title">Latlng : </span>${data1.latlng}</p>
+    <p><span class = "card-title">CountryCode : </span>${data1.cca3}</p>
+    <button type="button" class="btn btn-primary btn-lg" id "btn" onClick = "(displayWind('${data1.name.common}',${index}))">Click for Weather</button>
+    <p class="btn" style="display:none" id=btn${index}></p>
+    </div>
+    </div>
+    `
+  })
+  row.innerHTML = cardDetails;
+}
+
+
+
+function displayWind(name,index) {
+  let btn=document.getElementById('btn'+index)
+  let weather = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=a8fd5c805da21e8885ccf7f7f02f403e`);
+  weather.then((res1) => res1.json())
+    .then((data1) => {
+      btn.style.display="block"
+      btn.innerHTML = ` <p><i>'The Weather temp is :</i>${data1.main.temp}' <i>and Wind speed is :</i>${data1.wind.speed}</p>`
+    });
+
+}
