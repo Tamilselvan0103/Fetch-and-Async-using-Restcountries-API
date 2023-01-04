8f7334ec8e3ba956a21baf7881e45d5176874601
Tamilselvan0103 <tamilselvans0103@gmail.com>
2023-01-04 09:58:33 +0530

    updated

--- script.js
+++ script.js
@@ -14,16 +14,22 @@ restcountries();
 function showCountry(data1) {
   const cardDetails = data1.map((data1,index) => {
     return `
-    <div class="card content col-lg-4 col-sm-12" style="width: 18rem;">
+    <div class="col-sm-6 col-md-4 col-lg-4 col-xl-4" style="width: 18rem;">
+    <div class ="card h-100">
+    <div class ="card-header">
     <h1 class = "text-center" id ="title">${data1.name.common}</h1>
-    <img src="${data1.flags.png}" class="img-fluid img" alt="${data1.name.common}" >
+    </div>
+    <img src="${data1.flags.png}" class="img-fluid img card-img-top" alt="${data1.name.common}" >
     <div class="card-body">
+    <div class="card-text">
     <p><span class = "card-title">Capital : </span>${data1.capital}</p>
     <p><span class = "card-title">Region : </span>${data1.region}</p>
     <p><span class = "card-title">Latlng : </span>${data1.latlng}</p>
     <p><span class = "card-title">CountryCode : </span>${data1.cca3}</p>
     <button type="button" class="btn btn-primary btn-lg" id "btn" onClick = "(displayWind('${data1.name.common}',${index}))">Click for Weather</button>
     <p class="btn" style="display:none" id=btn${index}></p>
+    </div>
+    </div>
     </div>
     </div>
     `
